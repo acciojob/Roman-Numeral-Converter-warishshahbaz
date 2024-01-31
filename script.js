5609d6ba4983ff480e548569cbc1df62f6637830
warishshahbaz <95901915+warishshahbaz@users.noreply.github.com>
2024-01-31 17:51:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function convertToRoman(num) {
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-console.log(convertToRoman(36));
+// console.log(convertToRoman(36,"----"));
 
 
 
